44f78ee2c8e6c35af220d1bd73b292b1442d7ee5
Dhana027 <pradhanatafindyap@gmail.com>
2025-06-04 09:19:03 +0700

    Update TTTConsoleNonOO.java

--- TTTConsoleNonOO.java
+++ TTTConsoleNonOO.java
@@ -154,4 +154,17 @@ public class TTTConsoleNonOO {
             case NO_SEED: System.out.print("   "); break;
         }
     }
+    do {
+   // Play the game once
+   initGame();
+   ......
+   ......
+   // Prompt the user whether to play again
+   System.out.print("Play again (y/n)? ");
+   char ans = in.next().charAt(0);
+   if (ans != 'y' && ans != 'Y') {
+      System.out.println("Bye!");
+      System.exit(0);  // terminate the program
+   }
+} while (true);  // repeat until user did not answer yes
 }
